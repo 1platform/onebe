d75856aa59c64f8f28a70fdd4aafdff10d58cb9a
Valentin Duricu <valentin.duricu@spark-dev.ro>
2023-07-12 14:24:46 +0300

    Version bump to v2.7.4

--- src/version.test.ts
+++ src/version.test.ts
@@ -4,7 +4,7 @@ import BUILD from "@/build";
 
 describe("Check Version Information", () => {
   test("Application version should match what is defined in version", () => {
-    expect(getVersion()).toBe("2.7.3-DEV");
+    expect(getVersion()).toBe("2.7.4-DEV");
     expect(getVersion()).toBe(`${version}-${BUILD}`);
   });
   test("Application codename should match what is defined in version", () => {
@@ -12,7 +12,7 @@ describe("Check Version Information", () => {
     expect(getCodename()).toBe(codename);
   });
   test("Application codename should match what is defined in version", () => {
-    expect(getVersionCodename()).toBe("2.7.3-DEV (Rebuild)");
+    expect(getVersionCodename()).toBe("2.7.4-DEV (Rebuild)");
     expect(getVersionCodename()).toBe(`${version}-${BUILD} (${codename})`);
   });
 });
