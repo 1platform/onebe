c476eac1a594741fa18a8d9f2b92291d7eff05d9
Valentin Duricu <valentin.duricu@spark-dev.ro>
2023-07-12 14:53:29 +0300

    Fix some issues with Passport integration

--- src/Authentication/index.ts
+++ src/Authentication/index.ts
@@ -1,7 +1,7 @@
 export * from "@/Authentication/AuthDecorators";
 export * from "@/Authentication/JWT";
-export { passport, default as initPassportStrategy } from "@/Authentication/Passport";
-export type { IInitStrategyOptions } from "@/Authentication/Passport";
+export { passport, default as initPassportStrategy } from "@/Authentication/PassportIntegration";
+export type { IInitStrategyOptions } from "@/Authentication/PassportIntegration";
 
 export { default as AuthenticationMethod } from "@/Authentication/AuthenticationMethod";
 export type { default as IPayload } from "@/Authentication/IPayload";
