20bd169ba47da86e6517a50e75f47fe9ad167dc5
Valentin Duricu <valentin.duricu@spark-dev.ro>
2023-09-10 13:02:16 +0300

    Main fixes

--- src/version.ts
+++ src/version.ts
@@ -7,7 +7,7 @@ export const codename = "Rebuild";
 /**
  * The version of the framework.
  */
-export const version = "2.7.7";
+export const version = "2.7.8";
 
 /**
  * Function that returns the full version string (version + buildId).
