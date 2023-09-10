a7b54a70bbbdb5fdb9a1e95278e78126a07612a4
Valentin Duricu <valentin.duricu@spark-dev.ro>
2023-09-10 13:03:21 +0300

    Version bump to v2.7.9

--- src/version.ts
+++ src/version.ts
@@ -7,7 +7,7 @@ export const codename = "Rebuild";
 /**
  * The version of the framework.
  */
-export const version = "2.7.8";
+export const version = "2.7.9";
 
 /**
  * Function that returns the full version string (version + buildId).
